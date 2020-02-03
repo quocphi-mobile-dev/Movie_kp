139a178362ff563a2dfc35803d127ce3ca7a4db7
quocphineu97 <quocphibk1997@gmail.com>
2020-02-03 10:49:07 +0700

    handle data

---
+++ app/src/main/java/com/example/movie_kp/data/source/remote/Api.kt
@@ -0,0 +1,12 @@
+package com.example.movie_kp.data.source.remote
+
+object Api {
+    private const val API_KEY = "api key"
+    private const val URL = "https://api.themoviedb.org/3"
+    private const val BASE_POSTER_PATH = "https://image.tmdb.org/t/p/w342"
+    private const val BASE_BACKDROP_PATH = "https://image.tmdb.org/t/p/w780"
+    fun getApiKey() = API_KEY
+    fun getUrl() = URL
+    fun getPosterPath(posterPath: String) = BASE_POSTER_PATH + posterPath
+    fun getBackdropPath(backdropPath: String) = BASE_BACKDROP_PATH + backdropPath
+}
